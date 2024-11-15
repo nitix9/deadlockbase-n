7b584ee27345de7bc8370757ac7f1c5a1a810533
Nikita Romashko <romashko.04@mail.ru>
2024-11-15 18:54:09 +0500

    gh-pg

--- app.vue
+++ app.vue
@@ -5,12 +5,12 @@ useSeoMeta({
   ogTitle: 'Deadlockbase - всё об игре Deadlock',
   description: 'Новости, гайды на героев и многое другое по игре Deadlock.',
   ogDescription: 'Новости, гайды на героев и многое другое по игре Deadlock.',
-  ogImage: 'img/DEADLOCKBASE.png',
+  ogImage: 'image/DEADLOCKBASE.png',
   ogSiteName:'Deadlockbase',
   twitterCard: 'summary_large_image',
   twitterDescription:'Новости, гайды на героев и многое другое по игре Deadlock - многопользовательскому шутеру 6 на 6 с большим количеством героев, предметов, тактик и механик.',
   twitterTitle:'Новости, гайды на героев и многое другое по игре Deadlock.',
-  twitterImage:'img/DEADLOCKBASE.png',
+  twitterImage:'image/DEADLOCKBASE.png',
 })
 </script>
 <template>
