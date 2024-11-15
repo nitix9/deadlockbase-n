989cc654718fa3d17d7cde9d965fc82ff0e27798
Nikita Romashko <romashko.04@mail.ru>
2024-11-15 11:27:04 +0500

    Загрузка проекта на github

---
+++ components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template><footer><div class="info-footer"><p>Обратная связь</p>
+<p>Deadlockbase © 2024</p>
+</div></footer></template>
+<style>footer{color: black;border: rgba(255,255,255,0.05) 1px solid;}
+.info-footer{width: 85%;height: 10vhpx;display: flex;justify-content: space-between;align-items: center;margin: 0 auto;font-family: Montserrat; font-weight: 300;font-size: 14px;}
+.info-footer p{color: white;}
+</style>
---
+++ components/UpdateCard.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="update-card" v-for="n,index of cardInfo" v-bind:style="{background:'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,1) 100%),'+`url('${n}')`,
+backgroundRepeat:'no-repeat',backgroundPosition:'center'+' '+'center', backgroundSize:'cover',border:'rgba(255,255,255,0.2)'+' '+'solid'+' '+'1px'}">
+<div class="update-card_info">Обновление<p class="update-card__date">{{dateInf[index]}}</p></div></div>
+</template>
+<script>
+export default{
+    props:['cardInfo','dateInf']
+}
+</script>
+<style scoped>
+.update-card{color: white; display: flex;width: 50vh;height: 27vh;top: 0;left: 0;background-position: center center;background-size: cover;border-radius:25px;
+justify-content: center;align-items: flex-end;}
+.update-card_info{font-family:Montserrat;font-weight: 500;color: white;text-align: center;font-size: 1.5rem ;margin: 0;}
+.update-card__date{font-family: Playfair Display;color: white;font-size: 1.5rem;text-align: center;font-variant-numeric: lining-nums;padding-bottom: 15%;}
+p{margin: 0}
+</style>
